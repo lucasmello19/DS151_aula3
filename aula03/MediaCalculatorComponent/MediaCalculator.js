69842bf486e340fd017b0d09115a549d6e9160ce
Lucas Mello <lucasmello@MacBook-Pro-de-user.local>
2023-08-19 20:20:45 -0300

    update const

--- aula03/MediaCalculatorComponent/MediaCalculator.js
+++ aula03/MediaCalculatorComponent/MediaCalculator.js
@@ -1,3 +1,9 @@
+import React, { useState } from 'react';
+import styles from './MediaCalculatorStyles';
+
 export const MediaCalculator = () => {
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [result, setResult] = useState('');
 }
 
