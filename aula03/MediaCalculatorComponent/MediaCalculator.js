d2f6a2288562b1015608e099ff52bd5b9fc3e5b7
Fatima Kraiczyi <fatima.kraiczyi@brf.com>
2023-08-19 20:25:19 -0300

    basic media function

--- aula03/MediaCalculatorComponent/MediaCalculator.js
+++ aula03/MediaCalculatorComponent/MediaCalculator.js
@@ -5,5 +5,11 @@ export const MediaCalculator = () => {
     const [input1, setInput1] = useState('');
     const [input2, setInput2] = useState('');
     const [result, setResult] = useState('');
+
+    function calcularMedia() {
+        const nota1 = parseFloat(input1);
+        const nota2 = parseFloat(input2);
+        const resultado = (nota1 + nota2) / 2;
+    }
 }
 
