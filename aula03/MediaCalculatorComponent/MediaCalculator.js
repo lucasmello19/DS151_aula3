e19eda0d54425a5e3af062dc51f73abbf6d7aa4d
Lucas Mello <lucasmello@MacBook-Pro-de-user.local>
2023-08-19 21:00:31 -0300

    update input view

--- aula03/MediaCalculatorComponent/MediaCalculator.js
+++ aula03/MediaCalculatorComponent/MediaCalculator.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import styles from './MediaCalculatorStyles';
+import { StyleSheet, View, TextInput } from 'react-native';
 
-export const MediaCalculator = () => {
+
+const MediaCalculator = () => {
     const [input1, setInput1] = useState('');
     const [input2, setInput2] = useState('');
     const [result, setResult] = useState('');
@@ -11,5 +13,25 @@ export const MediaCalculator = () => {
         const nota2 = parseFloat(input2);
         const resultado = (nota1 + nota2) / 2;
     }
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                placeholder="Digite o primeiro número"
+                keyboardType="numeric"
+                value={input1}
+                onChangeText={text => setInput1(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Digite o segundo número"
+                keyboardType="numeric"
+                value={input2}
+                onChangeText={text => setInput1(text)}
+            />
+        </View>
+    )
 }
 
+export default MediaCalculator
--- aula03/MediaCalculatorComponent/MediaCalculatorStyles.js
+++ aula03/MediaCalculatorComponent/MediaCalculatorStyles.js
@@ -1,7 +1,19 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+});
 
 export default styles
